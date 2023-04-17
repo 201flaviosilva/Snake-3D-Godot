c69a3ad9db7a3e39d1d3210a18ceb29bed5af6f9
201flaviosilva <flavioandre.2001.9.7@gmail.com>
2023-04-18 00:55:13 +0100

    fix: player movimentation

--- Project/Game3D/Player/Player.gd
+++ Project/Game3D/Player/Player.gd
@@ -3,44 +3,50 @@ extends Area
 signal food_eated
 signal dead
 
-export (float, 0.1, 1) var normal_speed: float = 0.2
+export (float, 0.05, 1) var normal_speed: float = 0.1
 
-var direction: Vector3 = Vector3.FORWARD
+var direction: Vector3 = Vector3(0, 0, -1)
 var speed: float = normal_speed
 
+onready var CAMERA: Camera = get_parent().get_node("Camera")
+onready var CAMERA_TARGET: Position3D = get_node("Camera Target")
+onready var MOVE_TIMER: Timer = $Move
 
-func _ready() -> void:
+func _ready():
 	_set_speed(normal_speed)
-	pass
+
+	CAMERA.look_at(CAMERA_TARGET.global_transform.origin, Vector3.UP)
+	CAMERA.translation = CAMERA.translation.rotated(Vector3.UP, deg2rad(-45))
 
 func _process(delta: float) -> void:
-	if get_tree().paused: return  
+	if get_tree().paused: return;
+	look_at(global_transform.origin + direction, Vector3.UP)
+	
+	var camera_position = CAMERA_TARGET.global_transform.origin - direction * 10
+	CAMERA.translation = camera_position
+	CAMERA.look_at(CAMERA_TARGET.global_transform.origin, Vector3.UP)
 
-	if Input.is_action_just_pressed("up"): 
-		direction = Vector3.FORWARD
-		rotation_degrees.y = 0
+func _input(event):
+	if event.is_action_pressed("left"):
+		if direction.x == -1: direction = Vector3.BACK # Down
+		elif direction.x == 1: direction = Vector3.FORWARD # Up
+		elif direction.z == -1: direction = Vector3.LEFT # Left
+		elif direction.z == 1: direction = Vector3.RIGHT # Right
+	
+	if event.is_action_pressed("right"):
+		if direction.x == -1: direction = Vector3.FORWARD # Up
+		elif direction.x == 1: direction = Vector3.BACK # Down
+		elif direction.z == -1: direction = Vector3.RIGHT # Right
+		elif direction.z == 1: direction = Vector3.LEFT # Left
 		
-	if Input.is_action_just_pressed("down"): 
-		direction = Vector3.BACK
-		rotation_degrees.y = 180
-		
-	if Input.is_action_just_pressed("left"): 
-		direction = Vector3.LEFT
-		rotation_degrees.y = 90
-		
-	if Input.is_action_just_pressed("right"): 
-		direction = Vector3.RIGHT
-		rotation_degrees.y = -90
-
-	if Input.is_action_just_pressed("run"): _set_speed(normal_speed / 2)
-	elif Input.is_action_just_released("run"): _set_speed(normal_speed)
+	if event.is_action_pressed("run"): _set_speed(normal_speed / 2)
+	elif event.is_action_released("run"): _set_speed(normal_speed)
 
 func _on_Move_timeout() -> void:
 	if get_tree().paused: return
-	global_translation += direction
+	global_translate(direction)
 	
 func _set_speed(value: float) -> void:
-	var move_timer = $Move
 	speed = value
-	move_timer.wait_time = speed
-	move_timer.start()
+	MOVE_TIMER.wait_time = speed
+	MOVE_TIMER.start()
--- Project/UI/Pause.gd
+++ Project/UI/Pause.gd
@@ -4,4 +4,5 @@ func _ready() -> void:
 	hide()
 
 func _on_HomeButton_pressed() -> void:
+	get_tree().paused = false
 	get_tree().change_scene("res://UI/Menu.tscn")
