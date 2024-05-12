fc01ef5ac21a36d362afa23cd22338254399c474
201flaviosilva <flavioandre.2001.9.7@gmail.com>
2024-05-12 23:39:43 +0100

    refactor: 3d camera logic

--- Project/Game3D/Game3D.gd
+++ Project/Game3D/Game3D.gd
@@ -3,6 +3,21 @@ extends Spatial
 var score: int = 0
 var is_game_over: bool = false
 
+onready var CAMERA: Camera = $Camera
+onready var CAMERA_TARGET: Position3D = $"Player/Camera Target"
+onready var PLAYER: Player = $Player
+
+func _ready():
+	CAMERA.look_at(CAMERA_TARGET.global_transform.origin, Vector3.UP)
+	CAMERA.translation = CAMERA.translation.rotated(Vector3.UP, deg2rad(-45))
+
+func _process(delta: float) -> void:
+	if get_tree().paused: return;
+	
+	var camera_position = CAMERA_TARGET.global_transform.origin - PLAYER.direction * 10
+	CAMERA.translation = camera_position
+	CAMERA.look_at(CAMERA_TARGET.global_transform.origin, Vector3.UP)
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		if is_game_over:
--- Project/Game3D/Player/Player.gd
+++ Project/Game3D/Player/Player.gd
@@ -1,5 +1,7 @@
 extends Area
 
+class_name Player
+
 signal food_eated
 signal dead
 
@@ -8,23 +10,14 @@ export (float, 0.05, 1) var normal_speed: float = 0.1
 var direction: Vector3 = Vector3(0, 0, -1)
 var speed: float = normal_speed
 
-onready var CAMERA: Camera = get_parent().get_node("Camera")
-onready var CAMERA_TARGET: Position3D = get_node("Camera Target")
 onready var MOVE_TIMER: Timer = $Move
 
 func _ready():
 	_set_speed(normal_speed)
 
-	CAMERA.look_at(CAMERA_TARGET.global_transform.origin, Vector3.UP)
-	CAMERA.translation = CAMERA.translation.rotated(Vector3.UP, deg2rad(-45))
-
 func _process(delta: float) -> void:
 	if get_tree().paused: return;
 	look_at(global_transform.origin + direction, Vector3.UP)
-	
-	var camera_position = CAMERA_TARGET.global_transform.origin - direction * 10
-	CAMERA.translation = camera_position
-	CAMERA.look_at(CAMERA_TARGET.global_transform.origin, Vector3.UP)
 
 func _input(event):
 	if event.is_action_pressed("left"):
